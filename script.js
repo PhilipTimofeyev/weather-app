9e5d6ffe15c5b791244675ace3a71eac5e4fcef1
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-08-25 09:27:13 -0700

    Add function to filter specified weather attritubes

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ async function getWeatherInfo(city) {
 		let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=QPYDGJZBXPXX4YTKKEGPVTW68&contentType=json`, {mode: 'cors'})
 		let weatherData = await response.json();
 		console.log(weatherData)
+		filterResponse(weatherData)
 	} catch(err) {
 		// Do something
 	}
@@ -19,6 +20,19 @@ function searchCity() {
 	event.preventDefault()
 	const city = document.querySelector('input').value
 	getWeatherInfo(city)
+}
+
+function filterResponse(response) {
+	const allowedConditions = ['conditions', 'temp']
+	const currentConditions = response.currentConditions
+
+	const filtered = Object.fromEntries(
+		Object.entries(currentConditions).filter(
+			([key, value]) => allowedConditions.includes(key)
+			)
+		)
+
+	console.log(filtered)
 }
 
 
