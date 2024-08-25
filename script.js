2bbf6bf1ea5cd3daa2a56a6e5fa03f65d2779741
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-08-25 08:58:08 -0700

    Handle errors for api request

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
+
 async function getWeatherInfo(city) {
-	const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=QPYDGJZBXPXX4YTKKEGPVTW68&contentType=json`, {mode: 'cors'})
-	const weatherData = await response.json();
-	return weatherData
+	try {
+		let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=QPYDGJZBXPXX4YTKKEGPVTW68&contentType=json`, {mode: 'cors'})
+		let weatherData = await response.json();
+		console.log(weatherData)
+	} catch(err) {
+		// Do something
+	}
 }
 
-console.log(getWeatherInfo('cincinnati'))
+const data = getWeatherInfo('los angeles')
+
 
