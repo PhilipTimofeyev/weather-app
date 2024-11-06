fafccc29f720202483b2afd06bc47e27ccb7120d
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-11-05 17:33:29 -0800

    Fix method of converting between C and F, and change visibility of weather display

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 const searchBtn = document.querySelector('button')
 const fcToggle = document.getElementById('f-c-btn')
 const loader = document.querySelector('.loader')
+const weatherDisplay = document.querySelector('#weather-display')
 
-// console.log(loader.style.visibility = 'visible')
+let temp
 
 async function getWeatherInfo(city) {
 	try {
@@ -11,19 +12,19 @@ async function getWeatherInfo(city) {
 		let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=QPYDGJZBXPXX4YTKKEGPVTW68&contentType=json`, {mode: 'cors'})
 		let weatherData = await response.json();
 		weatherData = filterResponse(weatherData)
+		weatherDisplay.style.visibility = 'visible'
 		loader.style.visibility = 'hidden'
 		updateDOM(weatherData)
 	} catch(err) {
-			alert(err)
+		alert(err)
 	}
 }
-
 
 searchBtn.addEventListener('click', searchCity)
 fcToggle.addEventListener('click', toggleDegrees)
 
-function searchCity() {
-	event.preventDefault()
+function searchCity(e) {
+	e.preventDefault()
 	const city = document.querySelector('input').value
 	getWeatherInfo(city)
 }
@@ -40,15 +41,12 @@ function filterResponse(response) {
 	return filtered
 }
 
-
 function updateDOM(weatherData) {
 	const tempEl = document.getElementById('temp').children[1]
 	const conditionsEl = document.getElementById('conditions').children[1]
 
-	let temp = weatherData.temp
+	temp = weatherData.temp
 	let conditions = weatherData.conditions
-
-	console.log(tempEl)
 
 	tempEl.innerText = `${temp} F°`
 	conditionsEl.innerText = conditions
@@ -57,19 +55,14 @@ function updateDOM(weatherData) {
 function toggleDegrees() {
 	const tempEl = document.getElementById('temp').children[1]
 
-	let degree = tempEl.innerText.match(/\d*/)[0]
+	if (!tempEl.innerText) return
 
-	let cel = `${convertFtoC(degree)} C°`
-	let far = `${convertCtoF(degree)} F°`
+	let cel = `${convertFtoC()} C°`
+	let far = `${temp} F°`
 
 	tempEl.innerText = (tempEl.innerText.includes('F')) ? cel : far
 }
 
-function convertFtoC(far) {
-	return ((far - 32) * (5/9)).toFixed(1)
+function convertFtoC() {
+	return ((temp - 32) * (5/9)).toFixed(1)
 }
-
-function convertCtoF(cel) {
-	return ((cel * (9/5)) + 32).toFixed(1)
-}
-
